1c1c90730a8209f76f268224d2265a22b6d46da4
android9009 <egor200422@bk.ru>
2026-08-30 11:15:57 +0400

    Update gamesense.lua

--- gamesense.lua
+++ gamesense.lua
@@ -3101,6 +3101,7 @@ do
     Aimbot:Toggle({Name = "Auto fire", Flag = "AimbotAutoFire"})
     Aimbot:Toggle({Name = "Silent aim", Flag = "AimbotSilentAim"})
     Aimbot:Toggle({Name = "Anti recoil", Flag = "AimbotAntiRecoil"})
+    Aimbot:Toggle({Name = "Anti spread", Flag = "AimbotAntiSpread"})
     local RiskyColor = rgb(182, 182, 101)
     local AirShot = Aimbot:Toggle({Name = "Air shot", Flag = "AimbotAirShot"})
     AirShot.Items.Title.TextColor3 = RiskyColor
